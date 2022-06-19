6bceaf634ec9836474fe066ddb1b1c11a0b5beb9
Jakub Różycki <dev@jrozycki.me>
2022-06-19 17:44:34 +0200

    models and urls added

--- baseApp/models.py
+++ baseApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    # updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    warehouse = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- productTracker/urls.py
+++ productTracker/urls.py
@@ -14,17 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse("Home page")
-
-def about(request):
-    return HttpResponse("About page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('about/', about),
+    path('', include('baseApp.urls')),
 ]
